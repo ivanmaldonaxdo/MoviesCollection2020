e8f2828070bf5e66eeabb79eb001910ae1b866f7
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2020-12-07 18:05:11 -0300

    Admin.py corregido

--- SitioMCollections/PeliSeries/admin.py
+++ SitioMCollections/PeliSeries/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Categoria,Peliserie,Carousel,Consulta,Noticia,Favorito
+from .models import Categoria,Peliserie,Carousel,Consulta,Noticia
 # Register your models here.
-admin.site.register([Categoria,Peliserie,Carousel, Consulta,Noticia,Favorito])
+admin.site.register([Categoria,Peliserie,Carousel, Consulta,Noticia])
