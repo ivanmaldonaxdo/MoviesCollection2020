0ed7100ac0873530fbf1edbb404ae280d7a25506
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2020-12-08 16:34:13 -0300

    navegador toma el serviceworker.js

---
+++ SitioMCollections/serviceworker.js
@@ -0,0 +1,26 @@
+const cacheName = 'cacheMC';
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                '/',
+              '/offline/',
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, { ignoreSearch: true }))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
